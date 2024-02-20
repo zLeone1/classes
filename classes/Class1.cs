09b54be29f1dc95b182eb00df5aaff1244e0dc14
Roberto <Roberto@DESKTOP-1818MJE>
2024-02-19 20:44:36 -0600

    Agregar archivos de proyecto.

---
+++ classes/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    using System;
+
+    public class DateUtility
+    {
+        // Method to parse a string to a DateTime object
+        public static DateTime ParseDate(string dateString)
+        {
+            DateTime result;
+            if (DateTime.TryParse(dateString, out result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid date format");
+            }
+        }
+
+        // Method to calculate the difference between two dates
+        public static TimeSpan CalculateDateDifference(DateTime date1, DateTime date2)
+        {
+            return date2 - date1;
+        }
+
+        // Method to format a date as a string
+        public static string FormatDate(DateTime date, string format)
+        {
+            return date.ToString(format);
+        }
+    }
+
+}
---
+++ classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using classes;
+
+Console.WriteLine("Add Date in the format dd/MM/yyyy");
+string dateString = Console.ReadLine();
+DateTime parsedDate = DateUtility.ParseDate(dateString);
+Console.WriteLine($"Parsed Date: {parsedDate}");
+
+DateTime currentDate = DateTime.Now;
+TimeSpan difference = DateUtility.CalculateDateDifference(parsedDate, currentDate);
+Console.WriteLine($"Difference from parsed date to current date: {difference.Days} days");
+
+string formattedDate = DateUtility.FormatDate(currentDate, "dd/MM/yyyy");
+Console.WriteLine($"Formatted current date: {formattedDate}");
